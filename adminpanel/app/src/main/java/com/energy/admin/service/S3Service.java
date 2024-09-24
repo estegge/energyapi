de5f75e160fefbcf9ed268a0a03c3c0d0bd5a25a
Ella aan de Stegge <ella.aan.de.stegge@alliander.com>
2024-09-24 14:21:50 +0200

    Update S3Service.java

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,8 +16,8 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
+        String aws_access_key_id = "AKIA2UC3BSXMCGBW5BGK";
+        String aws_secret_access_key = "p15wyEn/8snIv+XeSPCzAsZT0R8JLqx96Q/fEc+4";
  
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
